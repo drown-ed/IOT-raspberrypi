638a9b7be33cbec06bdba74157d4338f0d97af78
drowned <fight1999club@gmail.com>
2023-06-08 17:12:57 +0900

    in raspyberry

---
+++ 230608/drawing.py
@@ -0,0 +1,30 @@
+#pygame drawing editor
+
+import pygame, sys
+from pygame.locals import *
+
+width, height = 500, 500
+radius = 0
+mX, mY = 0, 0
+
+pygame.init()
+wnd = pygame.display.set_mode((width, height))
+wnd.fill(pygame.Color(255, 255, 255))
+fps = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                wnd.fill(pygame.Color(255, 255, 255))
+        elif event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        mX, mY = pygame.mouse.get_pos()
+        b = pygame.mouse.get_pressed()
+        if b[0] == 1:
+            pygame.draw.circle(wnd,
+                               pygame.Color(255, 0, 0),
+                               (mX, mY), radius, radius)
+    pygame.display.update()
+    fps.tick(30)
